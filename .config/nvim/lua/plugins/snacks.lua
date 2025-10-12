e5b4ac528078ad508a560fade2983975adb6b290
Darren Victoriano <darren.victoriano@gmail.com>
2025-10-11 22:32:07 -0700

    updated snacks

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    -- stylua: ignore
+    keys = {
+    { "<leader><space>", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>ff", function() Snacks.picker.files({hidden = true}) end, desc = "Find Files (Root Dir)" },
+    { "<leader>fF", function() Snacks.picker.files({hidden = true, root = false}) end, desc = "Find Files (cwd)" },
+    },
+  },
+}
