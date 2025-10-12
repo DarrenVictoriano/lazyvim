fde33baa3fd8ab0e4063b95c46cc1f75fe1b0e57
Darren Victoriano <darren.victoriano@gmail.com>
2025-10-11 21:02:39 -0700

    added plugins

---
+++ .config/nvim/lua/plugins/oil-nvim.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional: for file icons
+    config = function()
+      local oil = require("oil")
+
+      oil.setup({
+        columns = { "icon" },
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+        },
+        float = {
+          padding = 2,
+          max_width = 100,
+          max_height = 30,
+        },
+        keymaps = {},
+      })
+
+      -- Open Oil in current file's directory
+      vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory (Oil)" })
+    end,
+  },
+}
