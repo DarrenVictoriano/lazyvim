98dd895350083d5d7951b6fdc6355050acdcb393
Darren Victoriano <darren.victoriano@gmail.com>
2026-01-16 11:33:31 -0800

    updated lazyvim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -5,12 +5,15 @@ return {
       pyright = function(_, opts)
         opts.capabilities = opts.capabilities or vim.lsp.protocol.make_client_capabilities()
         opts.capabilities.general = opts.capabilities.general or {}
-        opts.capabilities.general.positionEncodings = { "utf-8" }
+        opts.capabilities.general.positionEncodings = { "utf-16" }
       end,
       ruff = function(_, opts)
         opts.capabilities = opts.capabilities or vim.lsp.protocol.make_client_capabilities()
         opts.capabilities.general = opts.capabilities.general or {}
-        opts.capabilities.general.positionEncodings = { "utf-8" }
+        opts.capabilities.general.positionEncodings = { "utf-16" }
+      end,
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
       end,
     },
   },
