fddaf89f9a73719bf01b54155bc37cbc487efb96
Darren Victoriano <darren.victoriano@gmail.com>
2026-01-15 21:15:25 -0800

    autopairs with cmp

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -13,31 +13,25 @@ return {
     -- hook autopairs into cmp
     local cmp = require("cmp")
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- local handlers = require("nvim-autopairs.completion.handlers")
-    -- cmp.event:on(
-    --   "confirm_done",
-    --   cmp_autopairs.on_confirm_done({
-    --     filetypes = {
-    --       ["*"] = {
-    --         ["("] = {
-    --           kind = {
-    --             cmp.lsp.CompletionItemKind.Function,
-    --             cmp.lsp.CompletionItemKind.Method,
-    --           },
-    --           handler = handlers["*"],
-    --         },
-    --       },
-    --       tex = false,
-    --     },
-    --   })
-    -- )
+    local handlers = require("nvim-autopairs.completion.handlers")
 
     cmp.event:on(
       "confirm_done",
-      cmp_autopairs.on_confirm_done()
+      cmp_autopairs.on_confirm_done({
+        filetypes = {
+          ["*"] = {
+            ["("] = {
+              kind = {
+                cmp.lsp.CompletionItemKind.Function,
+                cmp.lsp.CompletionItemKind.Method,
+              },
+              handler = handlers["*"],
+            },
+          },
+          tex = false,
+        },
+      })
     )
-
 
     return opts
   end,
