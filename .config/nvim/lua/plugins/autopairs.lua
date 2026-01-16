0865531cd40267737ea3f82dd9f7cc7545eeb28b
Darren <write@darren.sh>
2026-01-15 21:16:23 -0800

    config: autopairs with cmp (#27)
    * autopairs with cmp
    
    * autopairs with cmp

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,38 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = true,
-  -- use opts = {} for passing setup options
-  -- this is equivalent to setup({}) function
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "windwp/nvim-autopairs",
+  },
+  opts = function(_, opts)
+    -- setup autopairs itself
+    require("nvim-autopairs").setup({
+      check_ts = true,
+      fast_wrap = {},
+    })
+
+    -- hook autopairs into cmp
+    local cmp = require("cmp")
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local handlers = require("nvim-autopairs.completion.handlers")
+
+    cmp.event:on(
+      "confirm_done",
+      cmp_autopairs.on_confirm_done({
+        filetypes = {
+          ["*"] = {
+            ["("] = {
+              kind = {
+                cmp.lsp.CompletionItemKind.Function,
+                cmp.lsp.CompletionItemKind.Method,
+              },
+              handler = handlers["*"],
+            },
+          },
+          tex = false,
+        },
+      })
+    )
+
+    return opts
+  end,
 }
---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,6 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    auto_brackets = { python = false } -- disables double brackets in python
+  }
+}
