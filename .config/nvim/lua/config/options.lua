a2ba962d309043d8e5919eed4e6c80e00cd3c2db
Darren Victoriano <darren.victoriano@gmail.com>
2026-02-08 16:02:22 -0800

    used blink cmp

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,3 @@
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
-
--- Set to `false` to prevent "non-lsp snippets"" from appearing inside completion windows
--- Motivation: Less clutter in completion windows and a more direct usage of snippets
-vim.g.lazyvim_mini_snippets_in_completion = true
