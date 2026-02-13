5424b0001f864af78f0438e6fbd74cb98404ad80
Darren Victoriano <darren.victoriano@gmail.com>
2026-02-12 21:51:16 -0800

    test

---
+++ .config/nvim/lua/plugins/_mini-hipattern.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "echasnovski/mini.hipatterns",
+    opts = function(_, opts)
+      local hipatterns = require("mini.hipatterns")
+
+      -- Simple HSL → HEX converter courtesy of AI
+      local function hsl_to_hex(h, s, l)
+        s = s / 100
+        l = l / 100
+
+        local c = (1 - math.abs(2 * l - 1)) * s
+        local x = c * (1 - math.abs((h / 60) % 2 - 1))
+        local m = l - c / 2
+
+        local r, g, b = 0, 0, 0
+
+        if h < 60 then
+          r, g, b = c, x, 0
+        elseif h < 120 then
+          r, g, b = x, c, 0
+        elseif h < 180 then
+          r, g, b = 0, c, x
+        elseif h < 240 then
+          r, g, b = 0, x, c
+        elseif h < 300 then
+          r, g, b = x, 0, c
+        else
+          r, g, b = c, 0, x
+        end
+
+        local function to_hex(n)
+          return string.format("%02x", math.floor((n + m) * 255))
+        end
+
+        return "#" .. to_hex(r) .. to_hex(g) .. to_hex(b)
+      end
+
+      -- Extend existing highlighters (do NOT overwrite)
+      opts.highlighters = opts.highlighters or {}
+
+      opts.highlighters.hsl_function = {
+        -- pattern for function call with literal int like: hslToHex(0, 100, 50)
+        pattern = "hslToHex%(%s*(%d+)%s*,%s*(%d+)%s*,%s*(%d+)%s*%)",
+        -- custom color group
+        group = function(_, match)
+          local h, s, l = match:match("(%d+)%s*,%s*(%d+)%s*,%s*(%d+)")
+          if not h then
+            return nil
+          end
+
+          local hex = hsl_to_hex(tonumber(h), tonumber(s), tonumber(l))
+          return hipatterns.compute_hex_color_group(hex, "bg")
+        end,
+      }
+
+      return opts
+    end,
+  },
+}
